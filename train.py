a495098d45e7881d657a97fc977e5722d02e114b
ak1ra-1 <gmax7052@gmail.com>
2026-08-29 17:54:12 +0300

    Update train.py

--- train.py
+++ train.py
@@ -10,10 +10,10 @@ from sklearn.ensemble import RandomForestClassifier
 def main():
     X, y = load_iris(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
+        X, y, test_size=0.1, random_state=42
     )
 
-    model = RandomForestClassifier(max_depth=2, random_state=42)
+    model = RandomForestClassifier(max_depth=6, random_state=42)
     model.fit(X_train, y_train)
 
     score = model.score(X_test, y_test)
